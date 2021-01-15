01f503ae487972a55f3e7b436c8a0770fba4acda
Christopher Maldonado <chmald@microsoft.com>
2021-01-15 17:20:35 -0600

    adding wp2 install

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 const wp = require('./scripts/wordpress');
+const wp2 = require('./scripts/wordpress2');
 
 app.get('/', (req, res) => {
     res.send('Hello Welcome to the jungle!')
@@ -17,6 +18,10 @@ app.get('/', (req, res) => {
 
 app.get('/wordpress-install/199fe551-4087-4034-95ec-af6b3248d65c', (req, res) => {
     res.render('wordpress/index');
+});
+
+app.get('/wordpress-install/0fd93714-b026-491e-901e-820419b615f9', (req, res) => {
+    res.render('wordpress2/index');
 });
 
 app.post('/wordpress-install/199fe551-4087-4034-95ec-af6b3248d65c', async(req, res) => {
@@ -39,7 +44,29 @@ app.post('/wordpress-install/199fe551-4087-4034-95ec-af6b3248d65c', async(req, r
             message: 'Site Name was not provided.'
         });
     }
-})
+});
+
+app.post('/wordpress-install/0fd93714-b026-491e-901e-820419b615f9', async(req, res) => {
+    if(req.body.siteName !== "" || null){
+        try {
+            const siteName = req.body.siteName;
+            await wp2.runInstall(siteName);
+            res.render('wordpress2/success', {
+                url: `https://${siteName}.azurewebsites.net/wp-login.php`
+            });
+        }
+        catch(err) {
+            console.log(err);
+            res.render('wordpress2/error', {
+                message: "Install failed or incomplete."
+            });
+        }
+    } else {
+        res.status(403).render('wordpress2/error', {
+            message: 'Site Name was not provided.'
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
---
+++ scripts/wordpress2.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+async function runInstall(siteName) {
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  const page = await browser.newPage();
+  await page.goto(`https://${siteName}.azurewebsites.net/`);
+  await Promise.all([
+      page.waitForNavigation(),
+      page.click('#language-continue')
+  ]);
+  await page.type('#weblog_title', 'AppService.L200.OSS.WordPress.1');
+  await page.type('#user_login', 'azuser');
+  await page.type('#pass1-text', 'Micr0s0ft');
+  await page.type('#admin_email', 'azuser@example.com');
+  await Promise.all([
+      page.waitForNavigation(),
+      page.click('#submit')
+  ]);
+
+  await browser.close();
+};
+
+module.exports.runInstall = runInstall;
